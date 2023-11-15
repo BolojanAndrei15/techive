422b76967fea83941e5d44b9cb8b303095856113
Bolojan Andrei <alexandru.andreibolojan@gmail.com>
2023-11-15 20:32:22 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Contact from "./Contact";
+import Footer from "./Footer";
+import Services from "./Services";
+import WorkProcces from "./WorkProcces";
+import TipeOfApps from "./TipeOfApps";
+import Products from "./Products";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <WorkProcces />
+      <Services />
+      <Contact />
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
